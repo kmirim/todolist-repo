5600335e0b331e779c41247ad8fc591d5f0cd3c9
Kaluanã Mirim <132582320+kmirim@users.noreply.github.com>
2025-08-08 09:28:53 -0300

    Update script.sh

--- terraform/script.sh
+++ terraform/script.sh
@@ -17,28 +17,20 @@ cd /home/ubuntu/meu-projeto
 
 # --- Configurar e iniciar o banco de dados PostgreSQL com Docker Compose ---
 echo "Iniciando o banco de dados com Docker Compose..."
-# Acessa a pasta onde está o docker-compose.yml
 cd db
 sudo docker-compose up -d
 
 # --- Configurar e iniciar a aplicação backend (Java Spring Boot) ---
 echo "Iniciando a aplicação backend..."
-# Acessa a pasta onde está o backend
 cd ../todolist-service # Ajuste o caminho se necessário
-# Compila e cria o .jar executável
 mvn clean install
-# Executa a aplicação Spring Boot de forma persistente
 nohup java -jar target/todolist-0.0.1-SNAPSHOT.jar > backend.log 2>&1 &
 
 # --- Configurar e iniciar a aplicação frontend (Next.js) ---
 echo "Iniciando a aplicação frontend..."
-# Acessa a pasta onde está o frontend
-cd ../todolist # Ajuste o caminho se necessário
-# Instala as dependências do Next.js
+cd ../todolist 
 npm install
-# Cria a build de produção
 npm run build
-# Inicia o servidor Next.js de forma persistente
 nohup npm start > frontend.log 2>&1 &
 
 echo "Instalação e inicialização concluídas!"
